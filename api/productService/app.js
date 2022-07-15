5cac55601f30315eaf953f4b0f61b1235fdf75c3
Huu Hoang <18120387@student.hcmus.edu.vn>
2022-07-15 22:15:43 +0700

    pull

--- api/productService/app.js
+++ api/productService/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const productRouter = require('./routes/product');
 const app = express();
 
@@ -31,7 +30,6 @@ mongoose.connect(config.URL_MONGODB, connectionParams)
   });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/productService', productRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/productService/routes/product.js
+++ api/productService/routes/product.js
@@ -6,4 +6,5 @@ const product = require('../controllers/productController')
 router.post('/postProduct', product.postProduct);
 router.post('/updateProduct', product.updateProduct);
 router.get('/getCommentProduct/:id', product.getCommentProduct);
+router.get('/listAll', product.listAll);
 module.exports = router;
